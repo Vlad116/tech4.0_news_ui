03ce6003c3edcfdb8c45711b96cf8fe81d2bc362
Алексеев Владислав Алексеевич <VAAlekseev@ibs.root.ibscorp.ru>
2022-10-08 01:37:56 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import About from "./About";
+// import Home from "./Home";
+
+const App: React.FC = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/about">
+        {/* <About /> */}
+      </Route>
+      <Route path="/">
+        {/* <Home /> */}
+      </Route>
+    </Routes>
+  </Router>
+);
+
+export default App;
